0943532aed3291a2d6a43993bbd7497cfdd86c12
KotatuBot <kotatubot@gmail.com>
2018-10-18 23:55:29 +0900

    CBC

--- mode.py
+++ mode.py
@@ -1,6 +1,15 @@
-class Mode(self):
+from convert import Convert_Bin
+
+class Mode():
     def __init__(self):
         pass
+
+    def encrypto(self,values):
+        return values
+
+    def decrypto(self,values):
+        return values
+
 
     def ECB_encrypto(self,value):
         encrypto_list = []
@@ -22,11 +31,37 @@ class Mode(self):
         for number,tip in enumerate(hira_value):
             if number == 0:
                 xor_result = IV^tip
-                encrypto_value = encrypto(xor_result)
+                encrypto_value = self.encrypto(xor_result)
                 encrypto_list.append(encrypto_value)
-            encrypto_value = encrypto_list[-1]^tip
-            encrypto_list.append(encrypto_value)
+            else:
+                encrypto_value = encrypto_list[-1]^tip
+                values = self.encrypto(encrypto_value)
+                encrypto_list.append(values)
         return encrypto_list
+
+    def CBC_decrypto(self,encrypto_value,IV):
+        decrypto_list = []
+        for number,tip in enumerate(encrypto_value):
+            decrypto_txt = self.decrypto(tip)
+            if number == 0:
+                xor_result = IV^decrypto_txt
+                decrypto_list.append(xor_result)
+            else:
+                # encrypto value ^ encrypto value +1
+                xor_result = encrypto_value[number-1]^decrypto_txt
+                decrypto_list.append(xor_result)
+
+        return decrypto_list
+
+
+if __name__ == "__main__":
+    mode = Mode()
+    cb = Convert_Bin()
+    value = cb.convert_bin("Hello")
+    test = mode.CBC_encrypto(value,15)
+    atai = mode.CBC_decrypto(test,15)
+    print(atai)
+
 
     
         
