ba2a5934048664cb16bb54acefd1d7d1821019f3
KotatuBot <kotatubot@gmail.com>
2018-10-22 20:19:57 +0900

    CTR,CBC

--- mode.py
+++ mode.py
@@ -1,5 +1,5 @@
-from convert import Convert_Bin
-
+from counter_n import Counter_N
+from convert import Convert
 class Mode():
     def __init__(self):
         pass
@@ -10,6 +10,11 @@ class Mode():
     def decrypto(self,values):
         return values
 
+    def IV_genrate(self,IV):
+        cb = Convert()
+        IV_de = cb.convert_bin(IV)
+        IV_list = [value[0] for value in IV_de]
+        return IV_list
 
     def ECB_encrypto(self,value):
         encrypto_list = []
@@ -30,11 +35,11 @@ class Mode():
         encrypto_list = []
         for number,tip in enumerate(hira_value):
             if number == 0:
-                xor_result = IV^tip
+                xor_result = [ i^v for(i,v) in zip(IV,tip)]
                 encrypto_value = self.encrypto(xor_result)
                 encrypto_list.append(encrypto_value)
             else:
-                encrypto_value = encrypto_list[-1]^tip
+                encrypto_value = [ e^v for(e,v) in zip(encrypto_list[-1],tip)]
                 values = self.encrypto(encrypto_value)
                 encrypto_list.append(values)
         return encrypto_list
@@ -44,26 +49,70 @@ class Mode():
         for number,tip in enumerate(encrypto_value):
             decrypto_txt = self.decrypto(tip)
             if number == 0:
-                xor_result = IV^decrypto_txt
+                xor_result = [i^d for(i,d) in zip(IV,decrypto_txt)]
                 decrypto_list.append(xor_result)
             else:
                 # encrypto value ^ encrypto value +1
-                xor_result = encrypto_value[number-1]^decrypto_txt
+                xor_result = [e^d for(e,d) in zip(encrypto_value[number-1],decrypto_txt)]
                 decrypto_list.append(xor_result)
 
         return decrypto_list
 
+    def CFB_encrypto(self,hira_value,IV):
+        encrypto_list = []
+        for number,tip in enumerate(hira_value):
+            if number == 0:
+                IV_en = self.encrypto(IV)
+                xor_result = IV_en^tip
+                encrypto_list.append(xor_result)
+            else:
+                encrypto_en = self.encrypto(encrypto_list[-1])
+                xor_result = encrypto_en^tip
+                encrypto_list.append(xor_result)
+        return encrypto_list
 
-if __name__ == "__main__":
-    mode = Mode()
-    cb = Convert_Bin()
-    value = cb.convert_bin("*&*#JLJFLJFlasjlfj5362")
-    test = mode.CBC_encrypto(value,15)
-    atai = mode.CBC_decrypto(test,15)
-    print(atai)
-    correct = cb.convert_str(atai)
-    print(correct)
+    def CFB_decrypto(self,encrypto_value,IV):
+        decrypto_list = []
+        for number,tip in enumerate(encrypto_value):
+            if number == 0:
+                IV_de =  self.decrypto(IV)
+                xor_result = IV_de^tip
+                decrypto_list.append(xor_result)
+            else:
+                decrypto_de = self.decrypto(decrypto_list[-1])
+                decrypto_value = decrypto_de^tip
+                decrypto_list.append(decrypto_value)
+        return decrypto_list 
 
 
-    
+    def OFB_mode(self,value,IV):
+        value_list = []
+        for number,tip in enumerate(value):
+            if number == 0:
+                IV_en = self.encrypto(IV)
+                xor_result = IV_en^tip
+                value_list.append(xor_result)
+                stream_key = IV_en
+            else:
+                stream_key = self.encrypto(stream_key)
+                xor_result = stream_key^tip
+                value_list.append(xor_result)
+        return value_list
+
+    def Counter_mode(self,value_l,Nonce_byte,key_length,block,mode="encrypto",stream_key=None):
+        if mode == "encrypto":
+            counter = Counter_N(Nonce_byte,key_length)
+            counter.noce_genrate()
+            counter.counter_block(block)
+            stream_key = counter.merge_block()
         
+        mode_list = []
+        for num in range(len(value_l)):
+            tmp_list = []
+            for nums in range(len(value_l[num])):
+                xor_result = value_l[num][nums]^stream_key[num][nums]
+                tmp_list.append(xor_result)
+            mode_list.append(tmp_list)
+
+        return mode_list,stream_key
+
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from convert import Convert
 from mode import Mode
+from crypto_exec import Crypto_Exec
 """
 test python
 """
@@ -7,12 +8,55 @@ test python
 def CBC_test1():
     mode = Mode()
     cb = Convert()
-    value = cb.convert_bin("*&*#JLJFLJFlasjlfj5362")
-    test = mode.CBC_encrypto(value,15)
-    atai = mode.CBC_decrypto(test,15)
-    print(atai)
+    ce = Crypto_Exec()
+    string = "THISAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+    b = ce.block_split(string)
+    value = cb.convert_bin(b)
+    IV = "B"*16
+    t = mode.IV_genrate(IV)
+    test = mode.CBC_encrypto(value,t)
+    atai = mode.CBC_decrypto(test,t)
     correct = cb.convert_str(atai)
     print(correct)
+
+def CFB_test():
+    mode = Mode()
+    cb = Convert()
+    value = cb.convert_bin("*&*#JLJFLJFlasjlfj5362")
+    test = mode.CFB_encrypto(value,15)
+    print(test)
+    correct = mode.CFB_decrypto(test,15)
+    print(correct)
+    t = cb.convert_str(correct)
+    print(t)
+
+
+def OFB_test():
+    mode = Mode()
+    cb = Convert()
+    value = cb.convert_bin("*&*#JLJFLJFlasjlfj5362")
+    encrypto = mode.OFB_mode(value,15)
+    print(encrypto)
+    decrypto = mode.OFB_mode(encrypto,15)
+    print(decrypto)
+    t = cb.convert_str(decrypto)
+    print(t)
+
+def CTR_test():
+    mode = Mode()
+    cb = Convert()
+    ce = Crypto_Exec()
+    string = "This is your name eding "
+    b = ce.block_split(string)
+    c = cb.convert_bin(b)
+    t,stream_key = mode.Counter_mode(c,8,128,8)
+    print("ango")
+    print(t)
+    v,test = mode.Counter_mode(t,8,128,8,mode="decrypto",stream_key=stream_key)
+    print("decrypto")
+    test = cb.convert_str(v)
+    print(test)
+
 
 
 if __name__ == "__main__":
